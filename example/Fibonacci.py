4eb7e9fd5f67dc7d3af505af899fd16c941f7f05
Jojo <sirjoandersonwf@gmail.com>
2024-03-11 22:17:26 -0300

    refactor: improve print function

--- example/Fibonacci.py
+++ example/Fibonacci.py
@@ -1,6 +1,6 @@
 def fibonacci(n):
     fib_series = [0, 1]
-    while len(fib_series) < n:
+    while fib_series[len(fib_series) - 1] <= n:
         fib_series.append(fib_series[-1] + fib_series[-2])
     return fib_series
 
@@ -8,6 +8,7 @@ def is_fibonacci(n):
     fib_series = [0, 1]
     if n in fib_series:
         return True
+    fib_series.append(fib_series[-1] + fib_series[-2])
     while fib_series[len(fib_series) - 1] <= n:
         if n in fib_series:
             return True
@@ -15,7 +16,7 @@ def is_fibonacci(n):
             fib_series.append(fib_series[-1] + fib_series[-2])
     return False
 
-num = 0
+num = 130
 print(f'is {num} in fibonacci')
-print(fibonacci(num / 3))
+print(fibonacci(num))
 print(is_fibonacci(num))
